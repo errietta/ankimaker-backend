d4063e9c117667bb9303b117771370de5738e009
Errietta Kostala <errietta@errietta.me>
2024-10-20 20:09:02 +0000

    move cors first!

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const OpenAI = require("openai");
+const cors = require('cors');
 
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const {
